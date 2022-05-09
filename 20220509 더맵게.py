afc11202ff9dd9ac9e868e332b6e710384e53bb1
Changjun Park <96476118+moveformyfuture@users.noreply.github.com>
2022-05-10 00:00:38 +0900

    Add files via upload

--- 20220509 더맵게.py
+++ 20220509 더맵게.py
@@ -1,17 +1,24 @@
+# 1. 우선순위 queue -> import heapq, hq.heapify(list)
+# 2. 정답 : cnt, 스코빌 지수를 K이상으로 만드는 최소 횟수
+# 3. 스코빌지수 계산을 위해 First, Second 만들기
+# 4. 스코빌지수 계산(hq.heappush)
+# 5. 스코빌지수 계산될때마다 cnt+1
+
 import heapq as hq
 
-def solution(scoville, K):
 
+def solution(scoville, K):
     hq.heapify(scoville)
-    answer = 0
+    cnt = 0
+
     while True:
-        first = hq.heappop(scoville)
-        if first >= K:
+        First = hq.heappop(scoville)
+        if First >= K:
             break
         if len(scoville) == 0:
             return -1
-        second = hq.heappop(scoville)
-        hq.heappush(scoville, first + second*2)
-        answer += 1
+        Second = hq.heappop(scoville)
+        hq.heappush(scoville, First + Second * 2)
+        cnt += 1
 
-    return answer
+    return cnt
